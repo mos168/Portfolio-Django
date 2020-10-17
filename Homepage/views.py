66c908f060ae290c53137e4cae7ce56cf0eae7a9
Humayun <Humayunah619@gmail.com>
2020-10-17 16:37:54 +0500

    Added requirements.txt file

--- Homepage/views.py
+++ Homepage/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def Homepage(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        fname = request.POST['first_name']
+        lname = request.POST['last_name']
+        email = request.POST['email_address']
+        message = request.POST['message']
+
+        print(fname, lname, email, message)
+        return redirect('Homepage')
+    else:
+        return render(request, 'index.html')
+    # return render(request, 'index.html')
